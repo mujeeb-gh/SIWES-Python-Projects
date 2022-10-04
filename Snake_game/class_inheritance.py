ca986bd49fd44641bbda213d1ceea28f4c0be5f4
mujeeb-gh <olamidebalogun174@gmail.com>
2022-10-04 14:22:44 +0100

    First release of projects

---
+++ Snake_game/class_inheritance.py
@@ -0,0 +1,13 @@
+class Animal:
+  def __init__(self):
+    self.num_eyes = 2
+  
+  def breathe(self):
+    print("Inhale...  Exhale.")
+    
+class Fish(Animal):
+  def __init__(self):
+    super().__init__()
+  
+  def swim(self):
+    print("moving in water...")
